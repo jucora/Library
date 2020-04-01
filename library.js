71f6b8b794edb3dc2e80e4038cbd659727a99006
Julian <artjulius@hotmail.com>
2020-04-01 10:40:01 -0500

    adding Bootstrap and styling

--- library.js
+++ library.js
@@ -1,16 +1,26 @@
-let myLibrary = ["Bible", "Harry Potter"];
+let myLibrary = ["Bible", "Harry Potter", "cars"];
 
 function Book() {}
 
 function addBookToLibrary() {}
 
+// function render() {
+//   let listItems = myLibrary.reduce((result, item) => {
+//     result += `<li>${item}</li>`;
+//     return result;
+//   }, "");
+//   let store = document.querySelector("#store");
+//   store.innerHTML = listItems;
+// }
+
 function render() {
-  let listItems = myLibrary.reduce((result, item) => {
-    result += `<li>${item}</li>`;
-    return result;
-  }, "");
   let store = document.querySelector("#store");
-  store.innerHTML = listItems;
+  myLibrary.forEach(function(element) {
+    store.innerHTML +=
+      element +
+      "<br>" +
+      "<form action='new.html'> <button>Remove</button></form>";
+  });
 }
 
 render();
