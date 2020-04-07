1648fd08098315761eedafe9a7cf64abbf22bdb1
Julian <artjulius@hotmail.com>
2020-04-06 21:44:22 -0500

    improving readme file and adding the last improvements

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Library
+<h1 align="center">Library Project</h1>
+
+<p align="center">
+  <img src="images/library.png">
+</p>
+
+## Description
+
+This project is a virtual Library when users can add books and mark the books already read. Users can also remove books from the store. This project is the first one for Javascript course, as part of the main Microverse curriculum.
+
+## Languages
+
+- HTML
+- CSS
+- Javascript
+
+## Framework
+
+- Bootstrap
+
+## Authors
+
+:guitar: Julian Belmonte
+
+- Github: [@jucora](https://github.com/jucora)
+- Twitter: [@Julian Belmonte](twitter.com/JulianBelmonte)
+- Linkedin: [Julian Belmonte](linkedin.com/in/julianbel)
--- js/library.js
+++ js/library.js
@@ -1,3 +1,110 @@
+var myLibrary = get_localstorage();
+document.addEventListener("DOMContentLoaded", ready);
+
+//FUNCTIONS
+
+function ready() {
+  if (myLibrary == null) {
+    myLibrary = [
+      {
+        title: "Harry Potter",
+        author: "J. K. Rowling",
+        number_of_pages: 200,
+      },
+      {
+        title: "Lord of rings",
+        author: "J. R. R. Tolkien",
+        number_of_pages: 300,
+      },
+      {
+        title: "The Hobbit",
+        author: "J. R. R. Tolkien",
+        number_of_pages: 150,
+      },
+      {
+        title: "Bible",
+        author: "No author",
+        number_of_pages: 500,
+      },
+    ];
+    myLibrary.forEach(function (book) {
+      book.read = false;
+    });
+  }
+  render(myLibrary);
+}
+
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.number_of_pages = pages;
+}
+
+Book.prototype.read = false;
+
+function render(myLibrary) {
+  myLibrary.forEach(function (element) {
+    var item = document.createElement("li");
+    item.classList.add("book");
+    draw_book(item, element);
+  });
+}
+
+function draw_book(item, element) {
+  var store = document.querySelector("#store");
+  let book_record;
+  let box_record;
+  if (element.read == true) {
+    book_record = "Read";
+    box_record = "checked = 'checked'";
+  } else {
+    book_record = "Not Read";
+    box_record = "";
+  }
+  item.innerHTML =
+    "<center><h2>" +
+    element.title +
+    "</h2></center>" +
+    "<center><img src= 'images/book_store.png' width='200px' class='book-cover'>" +
+    "<p>Author: " +
+    element.author +
+    "</p>" +
+    "<p>Pages: " +
+    element.number_of_pages +
+    "</p>" +
+    "<input type='checkbox' id = 'read-box' data-value='" +
+    element.title +
+    "'" +
+    box_record +
+    ">" +
+    "<label for='read' id = 'read-label' data-value='" +
+    element.title +
+    "'>" +
+    book_record +
+    "</label>\n" +
+    "<br/></br><button class = 'btn btn-primary remove' data-value='" +
+    element.title +
+    "'>Remove</button>";
+  store.appendChild(item);
+}
+
+function get_localstorage() {
+  myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+  return myLibrary;
+}
+
+function save_localstorage(myLibrary) {
+  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+  get_localstorage();
+}
+
+function check_message() {
+  let message = document.getElementById("submit-message");
+  if (document.contains(message)) {
+    message.remove();
+  }
+}
+
 //EVENT LISTENERS
 
 document.querySelector(".new").addEventListener("click", function () {
@@ -48,4 +155,33 @@ document.querySelector(".new").addEventListener("click", function () {
     }
     document.forms.newform.appendChild(message);
   });
+});
+
+store.addEventListener("click", function (e) {
+  if (e.target && e.target.matches("input")) {
+    let index = myLibrary
+      .map((book) => book.title)
+      .indexOf(e.target.dataset.value);
+    if (myLibrary[index].read == false) {
+      myLibrary[index].read = true;
+      e.target.nextSibling.innerHTML = "Read";
+    } else {
+      myLibrary[index].read = false;
+      e.target.nextSibling.innerHTML = "Not Read";
+    }
+    save_localstorage(myLibrary);
+  }
+});
+
+store.addEventListener("click", function (e) {
+  if (e.target && e.target.matches("button")) {
+    let index = myLibrary
+      .map((book) => book.title)
+      .indexOf(e.target.dataset.value);
+
+    myLibrary.splice(index, 1);
+
+    e.target.parentNode.parentNode.remove(e.target);
+    save_localstorage(myLibrary);
+  }
 });
